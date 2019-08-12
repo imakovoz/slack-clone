a3bdfceffc65ef527149e51fd93b9231052ff61f
Ilya Makovoz <imakovoz93@gmail.com>
2019-08-12 16:29:53 -0400

    setup base models

---
+++ backend/models/message.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure
+const DataSchema = new Schema(
+  {
+    id: Number,
+    message: String,
+    sender: Number,
+    Reciever: Number
+  },
+  { timestamps: true }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Data", DataSchema);
